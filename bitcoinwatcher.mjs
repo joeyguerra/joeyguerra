7290318bd33f68130ea5cccf60c11f4d3c5194a8
Joey Guerra <github@joeyguerra.com>
2025-02-19 07:21:13 -0600

    some updates

--- bitcoinwatcher.mjs
+++ bitcoinwatcher.mjs
@@ -1,16 +1,18 @@
-import { fstat } from "fs";
-import https from "https";
-import HttpAsync from "./lib/HttpAsync.mjs";
+import { fstat } from 'fs'
+import https from 'https'
 
-const bitcoinApiUrl = "https://api.coindesk.com/v1/bpi/currentprice.json";
+const bitcoinApiUrl = 'https://api.coindesk.com/v1/bpi/currentprice.json'
+
+function formatMoneyWithCommas(amount) {
+    return amount.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 4 })
+}
 
 setInterval(async ()=>{
     try {
-        const data = await HttpAsync.get(bitcoinApiUrl);
-        const doc = JSON.parse(data);
-        
-        console.log(`${doc.time.updated} - $${doc.bpi.USD.rate_float}`);
+        const doc = await (await fetch(bitcoinApiUrl)).json()
+        process.stdout.write('\r\x1b[K')
+        process.stdout.write(`${doc.time.updated} - $${formatMoneyWithCommas(doc.bpi.USD.rate_float)}\r`)
     }catch(e){
-        console.error(e);
+        console.error(e)
     }
-}, 5000);
+}, 5 * 1000)
--- pages/blog/2025/index.md
+++ pages/blog/2025/index.md
@@ -19,7 +19,7 @@ Identify data flows and processes and pick one to replace to start with. And the
         layout: './layouts/post.html',
         image: '',
         title: 'What do you do when the business case isn’t there but you know intuitively that building software will improve the bottom line?',
-        excerpt:"You start small and work outward from the middle",
+        excerpt:"You start small and work outward from the middle.",
         shouldPublish: true,
         published: new Date('2025-01-12T17:29:00.000Z'),
         tags: ['strategy', 'plan', 'software', 'business', 'case'],
