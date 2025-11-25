9eb9cf7f8f794b4f8bfc1036fb89b302edaa333e
Joey Guerra <github@joeyguerra.com>
2025-11-25 12:24:36 -0600

    softwar eguide

--- docker-build-k3d.sh
+++ docker-build-k3d.sh
@@ -4,4 +4,4 @@ npm version patch --no-git-tag-version
 VERSION=$(node -p "require('./package.json').version")
 sed -i '' -e "s|local/jbot-website:[0-9]*\.[0-9]*\.[0-9]*|local/jbot-website:$VERSION|g" charts/web/deployment.yaml
 docker build -t local/jbot-website:$VERSION .
-k3d image import local/jbot-website:$VERSION -c local
+k3d image import local/jbot-website:$VERSION -c ${KUBE_CLUSTER:-local}
