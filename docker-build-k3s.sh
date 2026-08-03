11e09db8e5ebd4518dd8d2693d192f9922a5729d
Joey Guerra <github@joeyguerra.com>
2026-08-02 21:08:46 -0500

    converted to k3s. cleaned up

--- docker-build-k3s.sh
+++ docker-build-k3s.sh
@@ -3,5 +3,5 @@ set -e
 npm version patch --no-git-tag-version
 VERSION=$(node -p "require('./package.json').version")
 sed -i '' -e "s|local/jbot-website:[0-9]*\.[0-9]*\.[0-9]*|local/jbot-website:$VERSION|g" charts/web/deployment.yaml
-docker build -t local/jbot-website:$VERSION .
+docker build --load -t local/jbot-website:$VERSION .
 docker save "local/jbot-website:$VERSION" | limactl shell "${LIMA_INSTANCE:-k3s}" -- sudo k3s ctr images import -
