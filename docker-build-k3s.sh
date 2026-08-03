079056d66ab2422f73213b118dc1b511d2f15efd
Joey Guerra <github@joeyguerra.com>
2026-08-02 21:14:08 -0500

    fix: moving to k3s

--- docker-build-k3s.sh
+++ docker-build-k3s.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
-npm version patch --no-git-tag-version
-VERSION=$(node -p "require('./package.json').version")
-sed -i '' -e "s|local/jbot-website:[0-9]*\.[0-9]*\.[0-9]*|local/jbot-website:$VERSION|g" charts/web/deployment.yaml
+export DOCKER_HOST="${DOCKER_HOST:-unix://${HOME}/.colima/default/docker.sock}"
+VERSION=$(git rev-parse --short HEAD)
+sed -i '' -e "s|local/jbot-website:[^ ]*|local/jbot-website:$VERSION|g" charts/web/deployment.yaml
 docker build --load -t local/jbot-website:$VERSION .
 docker save "local/jbot-website:$VERSION" | limactl shell "${LIMA_INSTANCE:-k3s}" -- sudo k3s ctr images import -
